fd24e7fa766149b7881361f8a3b4dcf3b14f4ff8
AQUATECHIE <amolequwam96@gmail.com>
2024-09-04 15:40:08 +0100

    auto-commit

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    var backgroundDiv = document.querySelector('.background-div');
-    
-    if (window.scrollY > 50) { // Adjust this value as needed
-        navbar.classList.add('active');
-        backgroundDiv.classList.add('active');
+    var navbar = document.getElementById('mainNavbar');
+    if (window.scrollY > document.querySelector('.carousel').offsetHeight) {
+        navbar.classList.add('navbar-active');
     } else {
-        navbar.classList.remove('active');
-        backgroundDiv.classList.remove('active');
+        navbar.classList.remove('navbar-active');
     }
 });
 
-// JavaScript to handle the menu toggle
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.getElementById('navLinks');
-
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
